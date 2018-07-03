a33d6241474d0eb6d13401448202e36dfd6db81e
lethanhsonthd <lethanhsonthd@aol.com>
2018-07-03 13:48:07 +0700

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const SHA256 = require('crypto-js/sha256')
+const blockchain = require('blockchain.info')
 class Block{
     constructor(index, timestamp, data, previoushash=''){
         this.index = index
@@ -6,9 +7,16 @@ class Block{
         this.data = data
         this.previoushash = previoushash
         this.hash = ''
+        this.nonce = 0
     }
     calculateHash(){
-        return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+    }
+    mineBlock(difficulty){
+        while (this.hash.substr(0, difficulty) !== Array(difficulty +1).join("0")){
+            this.nonce++
+            this.hash = this.calculateHash()
+        }
     }
 }
 class BlockChain{
@@ -43,4 +51,5 @@ class BlockChain{
 let mycoin = new BlockChain()
 mycoin.addBlock(new Block(1,'3/7/2018','data 1',{amount: 4}))
 mycoin.addBlock(new Block(2,'3/7/2018','data 2',{amount: 1}))
+console.log(Array(5).join('0'))
 console.log(mycoin.isChainValid())
